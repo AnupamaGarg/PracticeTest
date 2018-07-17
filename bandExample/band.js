98b6586faf1412ea280cfcba32bf05d92d119c16
anupamagarg <dolly.singla@gmail.com>
2018-07-17 12:38:17 -0500

    BattleOfBands

---
+++ bandExample/band.js
@@ -0,0 +1,16 @@
+var  bandNumber = 0 ;
+
+// const takeNumber = function (bandName) {
+//     bandNumber += 1 ;
+//    return (bandNumber +". " + bandName);
+
+// }
+
+// const scum = takeNumber("Galactic Scum")
+// console.log(scum);  // This should print "1. Galactic Scum" in the console
+
+// const under = takeNumber("Underdogs")
+// console.log(under);  // This should print "2. Underdogs" in the console
+
+// const rock = takeNumber("rockband")
+// console.log(rock); 
